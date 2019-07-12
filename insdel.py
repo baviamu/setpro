76b35697e19003f0cd6128277fa3ddbbe4ff85f8
baviamu <48151961+baviamu@users.noreply.github.com>
2019-07-12 15:59:24 +0530

    Create insdel.py

---
+++ insdel.py
@@ -0,0 +1,11 @@
+gg,dd=map(str,input().split())
+e=0
+if len(gg)>len(dd):
+   gg,dd=dd,gg
+r=0
+while r<len(gg):
+   e+=(ord(dd[r])-ord(gg[r]))
+   r+=1
+for r in range(r,len(dd)):
+   e+=ord(bb[r])-ord('a')+1
+print(e)
