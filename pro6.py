65e27fb3b3c7744f924816272d3918bf3e2b110a
baviamu <48151961+baviamu@users.noreply.github.com>
2019-07-25 20:59:16 +0530

    Update pro6.py

--- pro6.py
+++ pro6.py
@@ -1,9 +1,9 @@
 ft=int(input())
 dt=list(map(int,input().split()))
-g=0
+e=0
 for one in range(ft):
     for two in range(one,ft):  
         for three in range(two,ft):
             if set[one]<set[two]<set[three]:
-                g+=1
-print(g)
+                e+=1
+print(e)
