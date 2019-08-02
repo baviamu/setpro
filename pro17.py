487fb2a25e49d6f5c05c405d11a587ed589399d7
baviamu <48151961+baviamu@users.noreply.github.com>
2019-08-02 15:14:08 +0530

    Update pro17.py

--- pro17.py
+++ pro17.py
@@ -1,11 +1,16 @@
-xx,b=map(int,input().split())
-yy=list(map(int,input().split()))[:xx]
-cu=0
-for km in range(0,len(yy)-1):
-  for td in range(km+1,len(yy)-1):
-    if(yy[km]+yy[td]=b):
-      cu+=1  
-if (cu==1):
-  print("yes")
-else:
-  print("no")
+bn=input()
+gr=list(map(int,bn.split()))
+ktrf=gr[1]
+ol=input()
+flag=0
+vgo=list(map(int,ol.split()))
+for c in range(0,len(vgo)-1):
+	for z in range(c+1,len(vgo)):
+		if vgo[c]+vgo[z]==ktrf:
+			print("yes")
+			flag=1
+			break
+	if flag==1:
+		break
+if flag!=1:
+	print("no")
