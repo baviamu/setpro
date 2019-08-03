aa347841eb5e58de0ab262e625736fb9d462cd91
baviamu <48151961+baviamu@users.noreply.github.com>
2019-08-03 17:14:38 +0530

    Create pro25.py

---
+++ pro25.py
@@ -0,0 +1,13 @@
+mnr=input()
+level11=list(map(int,input().split()))
+maxu=0
+e=0
+while(e<len(level11)-1):
+    cnt=0
+    while(e<len(level11)-1 and level11[e]<level11[e+1]):
+        cnt+=1
+        e+=1
+    if(cnt>maxu):
+        maxu=count
+    e+=1
+print(maxu+1)
