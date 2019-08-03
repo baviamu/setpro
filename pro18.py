aad9fe6c0b75babcb8997cf4c18432d05a82bd04
baviamu <48151961+baviamu@users.noreply.github.com>
2019-08-03 16:15:08 +0530

    Update pro18.py

--- pro18.py
+++ pro18.py
@@ -1,17 +1,16 @@
-zz,cc=map(int,input().split())
-rf=[]
-for _ in range(zz):
-    rf.append(input())
-for w in range(len(rf)):
-    if('0' in rf[w]):
-        rf[w]=rf[w].replace('0','')
-    rf[w]=rf[w].replace(' ','')
-lengths=[]
-for w in rf:
-    if(len(w)>0):
-        lengths.append(len(w))
-cc=min(lengths)
-rtr1='1 '*cc
-rt1=rt1.strip()
-for w in range(cc):
-    print(rtr1)
+nn5,mm5=map(int,input().split())
+g=[]
+r=0
+for o in range(nn5):
+    g.append(list(map(int,input().split())))   
+for o in range(nn5):
+    for z in range(mm5-1):
+        for d in range(z+1,mm5+1):
+            if g[o][z:k]==[1]*len(g[o][z:d]):
+                 if all(g[b+o][z:d]==[1]*len(g[b+o][z:k]) for b in range(len(g[o][z:d])-1)):
+                     if len(g[o][z:d])>r:
+                        r=len(g[o][z:d])
+if len(g)==1 and len(g[0])==1 and g[0][0]==1:
+    print(1)
+for o in range(r):
+    print(*[1]*r) 
