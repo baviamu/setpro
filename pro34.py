8c2ebd3eea71d4db5d8e8efd4465a2016a60a96d
baviamu <48151961+baviamu@users.noreply.github.com>
2019-08-08 23:01:15 +0530

    Update pro34.py

--- pro34.py
+++ pro34.py
@@ -5,7 +5,7 @@ for w in range(bav):
       Liee14.append(input())
 for w in range(bav):
       for q in range(niv-1):
-            if(Liee14[w][j]!='R' and Liee14[w][q+1]!='R'):
+            if(Liee14[w][q]!='R' and Liee14[w][q+1]!='R'):
                   amu+=3
             elif(Liee14[w][q]!='G' and Liee14[w][q+1]!='G'):
                   amu+=5
