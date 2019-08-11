10d2c7c3f1def77085f34ccc3193a299439cdee4
baviamu <48151961+baviamu@users.noreply.github.com>
2019-08-11 23:46:51 +0530

    Update PRO37.py

--- PRO37.py
+++ PRO37.py
@@ -1,17 +1,17 @@
     
-err,Bbc=map(int,input().split())
+Bb1,Aa=map(int,input().split())
 w=list(map(int,input().split()))
 prq=list(map(int,input().split()))
 sr=[]
 tt=0
-for f in range(err):
+for f in range(Bb1):
     u=prq[f]/w[f]
     sr.append(u)
 while Bbc>=0 and len(sr)>0:
     mindex=sr.index(max(prq))
-    if Bbc>=w[mindex]:
+    if Aa>=w[mindex]:
         tt=tt+prq[mindex]
-        Bbc=Bbc-w[mindex]
+        Aa=Aa-w[mindex]
     w.pop(mindex)
     prq.pop(mindex)
     sr.pop(mindex)
