63c211f03e17d9efb1e028e97fba005f44bfed26
baviamu <48151961+baviamu@users.noreply.github.com>
2019-08-11 23:57:11 +0530

    Create pro39.py

---
+++ pro39.py
@@ -0,0 +1,11 @@
+ws = int(input())
+xc = int(ws/2)
+bg = []
+for u in range(ws, xc, -1):
+    v = str(u)
+    if i + sum([int(paa) for paa in v]) == ws:
+        print(1)
+        print(u)
+        break
+else:
+    print(0) 
