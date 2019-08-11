cff4d97105b8af042a9fa8688c262f791deffab0
baviamu <48151961+baviamu@users.noreply.github.com>
2019-08-11 23:58:57 +0530

    Update pro39.py

--- pro39.py
+++ pro39.py
@@ -3,7 +3,7 @@ xc = int(ws/2)
 bg = []
 for u in range(ws, xc, -1):
     v = str(u)
-    if i + sum([int(paa) for paa in v]) == ws:
+    if u + sum([int(paa) for paa in v]) == ws:
         print(1)
         print(u)
         break
