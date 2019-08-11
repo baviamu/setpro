6b491583b2f4103a771bf6e841d796fee597f94f
baviamu <48151961+baviamu@users.noreply.github.com>
2019-08-11 23:42:21 +0530

    Update PRO37.py

--- PRO37.py
+++ PRO37.py
@@ -1,19 +1,18 @@
-bavi=int(input())
-nivi=list(map(int,input().split()))
-buvi=0
-amu=0
-while(amu<len(nivi)):
-    w=nivi[amu]
-    if(amu==0):
-        if(len(nivi)==1):
-            e=1 
-            break
-    elif(amu==len(nivi)-1):
-        e=e
-    else:
-        if(w>nivi[amu+1] and w>nivi[amu-1]):
-            e=e+1
-        elif(w<nivi[amu-1] and w<nivi[amu+1]):
-            e=e+1
-    it=amu+1
-print(e)
+    
+err,Bbc=map(int,input().split())
+w=list(map(int,input().split()))
+prq=list(map(int,input().split()))
+sr=[]
+tt=0
+for f in range(err):
+    u=prq[f]/w[f]
+    sr.append(u)
+while Bbc>=0 and len(sr)>0:
+    mindex=sr.index(max(prq))
+    if Bbc>=w[mindex]:
+        tt=tt+prq[mindex]
+        Bbc=Bbc-w[mindex]
+    w.pop(mindex)
+    prq.pop(mindex)
+    sr.pop(mindex)
+print(tt)
